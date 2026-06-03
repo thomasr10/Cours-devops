d8f426aaa52c0521e1aeab6bcc2a1d7bc210dbe9
thomasr10 <t.rivoire01@gmail.com>
2026-06-03 12:29:29 +0200

    added db connection, .env.example

--- src/index.js
+++ src/index.js
@@ -1,9 +1,45 @@
-const app = require("express")();
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const taskRoutes = require('./routes/tasks');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+const errorHandler = require('./middlewares/errorHandler');
+const app = express();
 
-app.get("/", (req, res) => res.json({ message: "Bonjouuuur :)" }));
+const DB_URL = process.env.DB_URL;
+const PORT = process.env.PORT;
 
-const port = process.env.PORT || 6000;
+// Middleware
+app.use(helmet());
+app.use(cors());
 
-app.listen(port, () =>
-  console.log(`app listening on http://localhost:${port}`)
-);
+const connectDb = async () => {
+  try {
+    await mongoose.connect(DB_URL);
+    console.log('Connecté à MongoDB');
+  } catch(e) {
+    console.error(`Erreur lors de la connexion : ${e.message}`);
+    process.exit(1);
+  }
+}
+
+connectDb().
+  then(() => {
+    app.listen(PORT, () => console.log(`Serveur démarré : http://localhost:${PORT}`));
+  })
+  .catch(e => console.error(e));
+
+app.get('/health', (req, res) => {
+res.json({ status: 'ok', timestamp: new Date() });
+});
+
+app.get('/', (req, res) => {
+  res.json('Salut')
+})
+// Routes
+// app.use('/api/tasks', taskRoutes);
+// Error handling
+// app.use(errorHandler);
+module.exports = app;
